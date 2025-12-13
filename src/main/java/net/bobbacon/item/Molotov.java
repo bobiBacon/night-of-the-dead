9362b921a04555fb8c7258b32c528c19609a2239
bob_bacon <antoinedsa.6@gmail.com>
2025-12-13 15:20:30 -0500

    les items de molotov explosent meme au sol

--- src/main/java/net/bobbacon/item/Molotov.java
+++ src/main/java/net/bobbacon/item/Molotov.java
@@ -5,10 +5,7 @@ import net.bobbacon.entity.ModEntities;
 import net.bobbacon.entity.MolotovEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.FlyingItemEntity;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.thrown.PotionEntity;
@@ -27,6 +24,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
@@ -54,7 +52,9 @@ public class Molotov extends Item {
             itemStack.decrement(1);
             return TypedActionResult.success(itemStack, world.isClient());
         } else if (user.getStackInHand(Hand.OFF_HAND).isOf(Items.FLINT_AND_STEEL)) {
-            setLit(itemStack,true);
+            if (!world.isClient) {
+                setLit(itemStack, true);
+            }
             user.swingHand(Hand.OFF_HAND,!world.isClient);
             return TypedActionResult.fail(itemStack);
         }
@@ -64,6 +64,9 @@ public class Molotov extends Item {
     @Override
     public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(stack, world, entity, slot, selected);
+        if (world.isClient){
+            return;
+        }
         if (isLit(stack)){
             if (!selected){
                 setLit(stack,false);
@@ -72,20 +75,34 @@ public class Molotov extends Item {
             int time = getRemainingTime(stack);
             time--;
             if (time<=0){
-                if (!world.isClient){
                     stack.decrement(1);
-                    MolotovEntity molotovEntity = new MolotovEntity((LivingEntity) entity, world);
-                    molotovEntity.setItem(stack);
-                    molotovEntity.setPos(entity.getX(),entity.getY(),entity.getZ());
-                    world.spawnEntity(molotovEntity);
+                MolotovEntity molotovEntity = getMolotovEntity(stack, world, entity);
+                world.spawnEntity(molotovEntity);
                     molotovEntity.explode();
-                    entity.setOnFireFor(15);
-                }
+                    if (entity instanceof ItemEntity){
+                        entity.discard();
+                    }else {
+                        entity.setOnFireFor(10);
+                    }
 
             } else {
                 setRemainingTime(stack,time);
             }
         }
+    }
+
+    @NotNull
+    private static MolotovEntity getMolotovEntity(ItemStack stack, World world, Entity entity) {
+        MolotovEntity molotovEntity;
+        if (entity instanceof ItemEntity itemEntity){
+            molotovEntity = new MolotovEntity(itemEntity.getX(),itemEntity.getY(),itemEntity.getZ(), world);
+        }
+        else {
+            molotovEntity = new MolotovEntity((LivingEntity) entity, world);
+        }
+        molotovEntity.setItem(stack);
+        molotovEntity.setPos(entity.getX(), entity.getY(), entity.getZ());
+        return molotovEntity;
     }
 
 
@@ -111,6 +128,12 @@ public class Molotov extends Item {
     public static void setRemainingTime(ItemStack stack, int time) {
         NbtCompound nbtCompound = stack.getOrCreateNbt();
         nbtCompound.putInt(TIME_KEY,time);
+    }
+
+
+    @Override
+    public boolean allowNbtUpdateAnimation(PlayerEntity player, Hand hand, ItemStack oldStack, ItemStack newStack) {
+        return !oldStack.getOrCreateNbt().getBoolean(Lit_KEY);
     }
 
 }
